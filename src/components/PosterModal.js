3bfc167f63a39ad0e64ed05d860f92726d1bc159
Kelly Barber <kelly.james.barber@gmail.com>
2018-10-03 11:24:29 -0400

    Modal is working

--- src/components/PosterModal.js
+++ src/components/PosterModal.js
@@ -4,6 +4,9 @@ import { connect } from 'react-redux'
 class PosterModal extends Component {
   render() {
     const { poster, handleShowModal } = this.props
+    console.log(poster)
+    
+
     return (
       <div className='poster-modal' onClick={() => handleShowModal(null)}>
         <img className='poster-modal__image' src={poster.url} alt='Poster Image'/>
@@ -12,8 +15,17 @@ class PosterModal extends Component {
   }
 }
 
-const mapStateToProps = ({ posters: { posters } }, ownProps) => ({
-  poster: posters.filter(poster => poster._id === ownProps.contentId)
-})
+PosterModal.defaultProps = {
+  poster: {
+    url: ''
+  }
+}
+
+const mapStateToProps = ({ posters: { posters } }, ownProps) => {
+  const poster = posters.filter(poster => poster._id === ownProps.contentId)
+
+  if (!poster[0]) return {}
+  else return { poster: poster[0] }
+}
 
 export default connect(mapStateToProps)(PosterModal)
